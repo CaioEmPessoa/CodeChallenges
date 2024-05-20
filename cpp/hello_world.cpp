6b8e4936fe69305db42c1adf8e5e1d65f4fa4e98
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-05-20 18:57:02 -0300

    cpp hello world

---
+++ cpp/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+// int because the function returns an interger
+int main() {
+    const int numb = 1;
+    cout << "Hello World" << numb;
+    return 0;
+}
+
