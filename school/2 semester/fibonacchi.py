f870e4c2f04437a66387159c8544f722d9651a74
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-08-30 09:40:38 -0300

    updated fibonacchi file

--- school/2 semester/fibonacchi.py
+++ school/2 semester/fibonacchi.py
@@ -1,2 +1,14 @@
-# Make a program that returns the n number in fibonacchi sequence
-# ex: 2 = 1, 3 = 2
+from functools import lru_cache
+
+def fib(num):
+    if num <= 1:
+        return 0
+    elif num == 2:
+        return 1
+    else:
+        return fib(num-1)+fib(num-2)
+        
+print("oi")
+print(fib(500))
+print(fib(100))
+print(fib(500))
