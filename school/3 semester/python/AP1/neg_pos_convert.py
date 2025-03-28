38fb6e3a38e8349b8f6163a1e5fbb7be90ae9a1f
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-03-28 17:58:48 -0300

    updt neg_pos_convert

--- school/3 semester/python/AP1/neg_pos_convert.py
+++ school/3 semester/python/AP1/neg_pos_convert.py
@@ -1,4 +1,2 @@
-l = [1, 2, 3, 4]
-index_to_remove = 2  # Dynamic index
-l = l[:index_to_remove] + l[index_to_remove + 1:]  # Removes the item at the specified index
-print(l)
+def absolute(item) -> int:
+   return int((item**2)**0.5)
