188071362f7c387a2f22a5b57207100a9f1d8de1
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-06-06 13:38:40 -0300

    Now stringToInt supports any number from 0 to 1000

--- java/stringToInt/numberTextValues.java
+++ java/stringToInt/numberTextValues.java
@@ -7,20 +7,22 @@
 
 package stringToInt;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class numberTextValues{
 
     protected String value;
-    Map<String, String> textMap;
+    Map<String, String[]> textMap;
 
     public void generateArray() {
 
         String[] normalNumbs = {
             "zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",
             "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa",
-            "cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seissentos", "setessentos", "oitossentos", "novessentos"
+            "cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seicentos", "setecentos", "oitocentos", "novecentos",
+            "mil"
         };
 
         String[][] specialNumbs = {
@@ -32,30 +34,54 @@ public class numberTextValues{
         textMap = new HashMap<>();
 
         int numValue = 0;
-        for (int i = 0; i<normalNumbs.length; i++) {
-            textMap.put(normalNumbs[i], String.valueOf(numValue));
+        int digits = 1;
+        for (int i = 0; i < normalNumbs.length; i++) {
+            String[] list = {String.valueOf(numValue), String.valueOf(digits)};
+
+            textMap.put(normalNumbs[i], list);
+
             numValue++;
             if (numValue > 9) {
                 numValue = 1;
+                digits+=1;
             }
 
         }
 
         for (int i = 0; i < specialNumbs.length; i++) {
-            textMap.put(specialNumbs[i][0], specialNumbs[i][1]);
+            String[] numInfo = {specialNumbs[i][1], specialNumbs[i][2]};
+            textMap.put(specialNumbs[i][0], numInfo);
         }
 
     }
 
     public String getNumber(String value) {
         String[] numbers = value.split(" e ");
-        String finalNum = "";
+
+        int arraySize = Integer.parseInt(textMap.get(numbers[0])[1]);
+
+        char[] finalNum = new char[arraySize];
+        Arrays.fill(finalNum, '0');
 
         for (String numTxt : numbers) {
-            finalNum += textMap.get(numTxt);
+            int i = Integer.parseInt(textMap.get(numTxt)[1]);
+            int inv_i = Math.abs(i-arraySize);
+
+            char[] numValue = textMap.get(numTxt)[0].toCharArray();
+
+            if (numValue.length >= 2) {
+                finalNum = numValue;
+                break;
+            }
+
+            System.out.println(i);
+            System.out.println(inv_i);
+            System.out.println(arraySize);
+
+            finalNum[inv_i] = numValue[0];
         }
 
-        return finalNum;
+        return String.valueOf(finalNum);
     }
 
     public void main(String[] args) {
