c72c636d4719cc3bf9b9f9cc59977100bd052583
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-03-05 17:27:03 -0300

    updt labirinto

--- school/5 semester/IA/labirinto.py
+++ school/5 semester/IA/labirinto.py
@@ -16,15 +16,51 @@ LABIRINTO = [
     [".", ".", ".", ".", ".", ".", ".", ".", "."],
 ]
 
+lab = LABIRINTO
+
 # Gerar movimentos válidos a partir de uma posicao
+def check_valid(pos):
+    print("Checking", pos)
+    valid_blocks = []
+
+    x_size = len(lab[0])
+    y_size = len(lab)
+
+    x, y = pos
+
+    around = [
+        (x  , y+1), # cima
+        (x+1, y  ), # direita
+        (x  , y-1), # baixo
+        (x-1, y  ), # esquerda
+        ]
+
+    for valid_x, valid_y in around:
+        if 0 <= valid_x <= x_size and 0 <= valid_y <= y_size:
+            check_block = lab[valid_x][valid_y]
+            if check_block not in ["#", "E"]:
+                print(f"Valid: {valid_x}:{valid_y} '{check_block}'" )
+                valid_blocks.append((valid_x, valid_y))
+
+    return valid_blocks
 
 # Distancia de manhatan entre dois pontos
-
+def manhatan_distance(pos1, pos2):
+    x1, y1 = pos1
+    x2, y2 = pos2
+    m = abs(x1 - y1) + abs(x2 - x2)
+    return m
 
 # ======= ALGORITIMOS DE BUSCA =======
 # bfs
+def bfs():
+    # explorar todas casas adjascentes antes de explorar um nivel a +
+    return
 
 # dfs
+def dfs():
+    # explorar um caminho ate o fim.
+    return
 
 # Busca Gulosa
 
