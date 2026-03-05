2aba0bcc68ed7c2f4c9fb41ff9340272aec856cb
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-03-04 22:02:49 -0300

    added base for labirinto

---
+++ school/5 semester/IA/labirinto.py
@@ -0,0 +1,43 @@
+# S = INICIO
+# . = LIVRE
+# E = INIMIGO (IGUAL PAREDE K)
+# # = PAREDE
+# G = FIM
+
+LABIRINTO = [
+    ["S", ".", "#", ".", ".", ".", "#", ".", "G"],
+    [".", "#", ".", "#", ".", "#", ".", "#", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", "#", ".", "#", ".", "#", ".", "#", "."],
+    [".", ".", ".", ".", "E", ".", ".", ".", "."],
+    [".", "#", ".", "#", ".", "#", ".", "#", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", "#", ".", "#", ".", "#", ".", "#", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+]
+
+# Gerar movimentos válidos a partir de uma posicao
+
+# Distancia de manhatan entre dois pontos
+
+
+# ======= ALGORITIMOS DE BUSCA =======
+# bfs
+
+# dfs
+
+# Busca Gulosa
+
+# Minimax
+
+
+# ======= FUNCOES DE TESTE =======
+# Testar cada algortimo com diferentes configs do labirinto
+
+# Comparar o desempenho dos algoritimos em termo de passos, tempo de execucao e eficiencia de evitar inimigos
+
+
+# ======= VISUALIZACAO =======
+# Imprimir o labirinto no console
+
+# Exibir o caminho encontrado por cada algoritimo
