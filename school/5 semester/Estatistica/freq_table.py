389342526efa09b35e31160fd5f6b27846e77546
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-04-29 02:08:31 -0300

    refactored minor use details in freq_table

--- school/5 semester/Estatistica/freq_table.py
+++ school/5 semester/Estatistica/freq_table.py
@@ -1,18 +1,17 @@
 import math
 
-valores_str = "450; 468; 476; 480; 482; 485; 488; 490; 492; 494; 496; 498; 499; 501; 502; 504; 505; 506; 507; 508; 509; 510; 510; 511; 512; 513; 514; 515; 515; 516; 517; 518; 519; 520; 520; 521; 522; 523; 524; 524; 525; 525; 526; 527; 528; 529; 530; 530; 531; 532; 533; 534; 535; 535; 536; 537; 538; 539; 540; 540; 541; 542; 544; 545; 546; 548; 549; 550; 552; 554; 556; 558; 560; 562; 565; 568; 570; 574; 582; 600"
+valores_str = """450; 468; 476; 480; 482; 485; 488; 490; 492; 494; 496; 498; 499; 501; 502; 504; 505; 506; 507; 508; 509; 510; 510; 511; 512; 513; 514; 515; 515; 516; 517; 518; 519; 520;520; 521; 522; 523; 524; 524; 525; 525; 526; 527; 528; 529; 530; 530; 531; 532; 533;534; 535; 535; 536; 537; 538; 539; 540; 540; 541; 542; 544; 545; 546; 548; 549; 550;552; 554; 556; 558; 560; 562; 565; 568; 570; 574; 582; 600"""
 
-valores = [int(e) for e in valores_str.split("; ")]
+valores = [int(e) for e in valores_str.split(";")]
 
 sturges = round(1 + 3.3 * math.log10(len(valores)))
 
 amplitude_total = math.ceil(max(valores) - min(valores))
 amplitude_classe = math.ceil(amplitude_total/sturges)
 
-print(f"amplitude total: {amplitude_total}")
 
 
-numero_ideal_classes = amplitude_total / sturges
+numero_ideal_classes = math.ceil(amplitude_total / sturges)
 
 '''
 {
@@ -48,6 +47,7 @@ for v in range(sturges):
     })
 
 print(valores)
+print(f"amplitude total: {amplitude_total}")
 print(f"sturges - numero ideal de classes: {sturges}")
 print(f"sturges - intervalo de classes: {numero_ideal_classes}")
 
